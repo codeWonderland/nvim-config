cb92c5da2b220518264ad3b00b48cc7c74d5fa5c
codeWonderland <alice.easterdevelops@gmail.com>
2025-04-15 18:17:59 -0400

    setting basic config settings and per language settings

--- init.lua
+++ init.lua
@@ -3,6 +3,10 @@ require("config.lazy")
 
 -- editor preferences
 vim.cmd.colorscheme("habamax")
+
+vim.o.number = true
+vim.o.relativenumber = true
+
 vim.o.tabstop = 4 -- a TAB character looks like 4 spaces
 vim.bo.tabstop = 4
 vim.o.expandtab = true -- Pressing a TAB key will insert spaces
