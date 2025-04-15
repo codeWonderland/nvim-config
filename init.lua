4c7a3cad8fc7154be6bfff6f993fcf0ea42c2677
codeWonderland <alice.easterdevelops@gmail.com>
2025-04-15 19:59:51 -0400

    added treesitter and mini configs

--- init.lua
+++ init.lua
@@ -4,13 +4,7 @@ require("config.lazy")
 -- editor preferences
 vim.cmd.colorscheme("habamax")
 
-vim.o.number = true
-vim.o.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
-vim.o.tabstop = 4 -- a TAB character looks like 4 spaces
-vim.bo.tabstop = 4
-vim.o.expandtab = true -- Pressing a TAB key will insert spaces
-vim.o.softtabstop = 4 -- number of spaces inserted on TAB
-vim.o.shiftwidth = 4 -- number of spaces inserted when indenting
-vim.bo.shiftwidth = 4
-vim.o.shiftround = true
+vim.opt.shiftwidth = 4 -- number of spaces inserted when indenting
