4d06da797d1ec4e43c8969901c92040fc54f8071
codeWonderland <alice.easterdevelops@gmail.com>
2025-04-16 11:48:31 -0400

    lsp setup for lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,9 +1,9 @@
 -- lua/plugins/mason.lua
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end
-	}
+    {
+	"williamboman/mason.nvim",
+	config = function()
+	    require("mason").setup()
+	end
+    }
 }
