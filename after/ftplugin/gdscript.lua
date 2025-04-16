06172b159cc2e106578e856fcec47461a5ff85f6
codeWonderland <alice.easterdevelops@gmail.com>
2025-04-16 10:48:58 -0400

    Updated gdscript settings to be clearer

--- after/ftplugin/gdscript.lua
+++ after/ftplugin/gdscript.lua
@@ -7,8 +7,8 @@ vim.api.nvim_create_autocmd(
         group = "AutoFormat",
         callback = function()
             local current_file = vim.api.nvim_buf_get_name(0) -- Get the full path of the current buffer.
-            local command = "gdformat " .. vim.fn.shellescape(current_file) -- shellescape the file path.
-            vim.fn.system(command) -- execute the format command
+            local format_command = "gdformat " .. vim.fn.shellescape(current_file) -- shellescape the file path.
+            vim.fn.system(format_command) -- execute the format command
 	    vim.cmd("edit") -- Reload the file
 
 	    -- Lint the file and print the output
