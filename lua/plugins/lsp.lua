dc3f1211d30f59400167156d830db44126ff55b2
codeWonderland <alice.easterdevelops@gmail.com>
2025-04-19 12:22:12 -0400

    Added lsp settings for gdscript

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
     {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-	    { 
+	    {
 		"folke/lazydev.nvim",
 		ft = "lua", -- only load on lua files
 		opts = {
@@ -15,7 +15,8 @@ return {
 	    },
 	},
 	config = function()
-	    require("lspconfig").lua_ls.setup {} 
+	    require("lspconfig").lua_ls.setup {}
+	    require("lspconfig").gdscript.setup {}
 	end
     }
 }
