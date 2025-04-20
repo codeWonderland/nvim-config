94fed4c899e0075fe39edbdcaa6ab3497808c103
codeWonderland <alice.easterdevelops@gmail.com>
2025-04-20 18:14:59 -0400

    plugin updates

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,12 +1,12 @@
 -- lua/plugins/oil.lua
 return {
-		{
-				"stevearc/oil.nvim",
-				-- Optional dependencies
-				dependencies = { "nvim-tree/nvim-web-devicons" },
-				config = function()
-						require("oil").setup()
-				end,
-				lazy = false,
-		}
+	{
+		"stevearc/oil.nvim",
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup()
+		end,
+		lazy = false,
+	}
 }
